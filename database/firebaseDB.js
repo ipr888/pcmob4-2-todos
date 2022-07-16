6ef1c42757648173a40b434696b0110463f0beb7
ipr888 <80932448+ipr888@users.noreply.github.com>
2022-07-16 19:14:34 +0800

    Add files via upload

--- database/firebaseDB.js
+++ database/firebaseDB.js
@@ -1,1 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDod3Y0U8Y7D13WbhCxpx0CherVfTVx9ys",
+  authDomain: "pcmob4-96963.firebaseapp.com",
+  projectId: "pcmob4-96963",
+  storageBucket: "pcmob4-96963.appspot.com",
+  messagingSenderId: "1030215658187",
+  appId: "1:1030215658187:web:5fd378ca20f06d0cfe094e",
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
